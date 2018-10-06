244dc8115fc40a399b7eb9e7ef139eb555bbaf33
Vitolo, Matteo, Vodafone Italy <Matteo.Vitolo@Vodafone.com>
2018-10-06 23:30:15 +0200

    corrected test

--- test/speedometer_test.dart
+++ test/speedometer_test.dart
@@ -1,7 +1,5 @@
 import 'package:test/test.dart';
 
-import 'package:speedometer/speedometer.dart';
-
 void main() {
   test('adds one to input values', () {
 //    final calculator = new Calculator();
