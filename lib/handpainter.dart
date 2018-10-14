c2d49f9756e3b6a68b9e46fd4eb0f6c764f218a4
Vitolo, Matteo, Vodafone Italy <Matteo.Vitolo@Vodafone.com>
2018-10-14 09:14:34 +0200

    corrected identation

--- lib/handpainter.dart
+++ lib/handpainter.dart
@@ -3,49 +3,49 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class HandPainter extends CustomPainter{
-  final Paint minuteHandPaint;
-  double value;
-  int start;
-  int end;
-  Color color;
-
-  HandPainter({this.value,this.start,this.end,this.color}):minuteHandPaint= new Paint(){
-    minuteHandPaint.color= this.color;
-    minuteHandPaint.style= PaintingStyle.fill;
-
-  }
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final radius= size.width/2;
-
-    double downSizedValue = ((value <= (this.end/2))? value : value - (this.end/2))*(40/this.end);
-    double realValue = (((value <= (this.end/2))? downSizedValue+40 : downSizedValue)%this.end);
-
-    canvas.save();
-
-    canvas.translate(radius, radius);
-
-    canvas.rotate(2*pi*((realValue)/this.end));
-
-    Path path= new Path();
-    path.moveTo(-1.5, -radius-10.0);
-    path.lineTo(-5.0, -radius/1.8);
-    path.lineTo(-2.0, 10.0);
-    path.lineTo(2.0, 10.0);
-    path.lineTo(5.0, -radius/1.8);
-    path.lineTo(1.5, -radius-10.0);
-    path.close();
-
-    canvas.drawPath(path, minuteHandPaint);
-    canvas.drawShadow(path, this.color, 4.0, false);
-
-    canvas.restore();
-
-  }
-
-  @override
-  bool shouldRepaint(HandPainter oldDelegate) {
-    return true;
-  }
+    final Paint minuteHandPaint;
+    double value;
+    int start;
+    int end;
+    Color color;
+    
+    HandPainter({this.value,this.start,this.end,this.color}):minuteHandPaint= new Paint(){
+        minuteHandPaint.color= this.color;
+        minuteHandPaint.style= PaintingStyle.fill;
+        
+    }
+    
+    @override
+    void paint(Canvas canvas, Size size) {
+        final radius= size.width/2;
+        
+        double downSizedValue = ((value <= (this.end/2))? value : value - (this.end/2))*(40/this.end);
+        double realValue = (((value <= (this.end/2))? downSizedValue+40 : downSizedValue)%this.end);
+        
+        canvas.save();
+        
+        canvas.translate(radius, radius);
+        
+        canvas.rotate(2*pi*((realValue)/this.end));
+        
+        Path path= new Path();
+        path.moveTo(-1.5, -radius-10.0);
+        path.lineTo(-5.0, -radius/1.8);
+        path.lineTo(-2.0, 10.0);
+        path.lineTo(2.0, 10.0);
+        path.lineTo(5.0, -radius/1.8);
+        path.lineTo(1.5, -radius-10.0);
+        path.close();
+        
+        canvas.drawPath(path, minuteHandPaint);
+        canvas.drawShadow(path, this.color, 4.0, false);
+        
+        canvas.restore();
+        
+    }
+    
+    @override
+    bool shouldRepaint(HandPainter oldDelegate) {
+        return true;
+    }
 }
--- lib/speedometer.dart
+++ lib/speedometer.dart
@@ -9,125 +9,125 @@ import 'package:speedometer/speedtextpainter.dart';
 import 'package:rxdart/rxdart.dart';
 
 class SpeedOMeter extends StatefulWidget {
-
-  int start;
-  int end;
-  double highlightStart;
-  double highlightEnd;
-  ThemeData themeData;
-
-  PublishSubject<double> eventObservable;
-  SpeedOMeter({this.start,this.end,this.highlightStart,this.highlightEnd,this.themeData, this.eventObservable}){
-    print(this.highlightEnd);
-
-  }
-
-  @override
-  _SpeedOMeterState createState() => new _SpeedOMeterState(this.start,this.end,this.highlightStart,this.highlightEnd,this.eventObservable);
+    
+    int start;
+    int end;
+    double highlightStart;
+    double highlightEnd;
+    ThemeData themeData;
+    
+    PublishSubject<double> eventObservable;
+    SpeedOMeter({this.start,this.end,this.highlightStart,this.highlightEnd,this.themeData, this.eventObservable}){
+        print(this.highlightEnd);
+        
+    }
+    
+    @override
+    _SpeedOMeterState createState() => new _SpeedOMeterState(this.start,this.end,this.highlightStart,this.highlightEnd,this.eventObservable);
 }
 
 class _SpeedOMeterState extends State<SpeedOMeter>  with TickerProviderStateMixin{
-  int start;
-  int end;
-  double highlightStart;
-  double highlightEnd;
-  PublishSubject<double> eventObservable;
-
-  double val = 0.0;
-  double newVal;
-  AnimationController percentageAnimationController;
-
-
-  _SpeedOMeterState(int start, int end, double highlightStart, double highlightEnd, PublishSubject<double> eventObservable) {
-    this.start = start;
-    this.end = end;
-    this.highlightStart = highlightStart;
-    this.highlightEnd = highlightEnd;
-    this.eventObservable = eventObservable;
-
-    percentageAnimationController = new AnimationController(
-        vsync: this,
-        duration: new Duration(milliseconds: 1000)
-    )
-      ..addListener((){
-        setState(() {
-          val = lerpDouble(val,newVal,percentageAnimationController.value);
-        });
-      });
-    this.eventObservable.listen((value) => reloadData(value));
-  }
-
-  reloadData(double value){
-    print(value);
-    newVal = value;
-    percentageAnimationController.forward(from: 0.0);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Center(
-      child: new LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            return new Container(
-              height: constraints.maxWidth,
-              width: constraints.maxWidth,
-              child: new Stack(
-                  fit: StackFit.expand,
-
-                  children: <Widget>[new Container(
-                    child: new CustomPaint(
-                        foregroundPainter: new LinePainter(
-                            lineColor:this.widget.themeData.backgroundColor,
-                            completeColor: this.widget.themeData.primaryColor,
-                            startValue: this.start,
-                            endValue: this.end,
-                            startPercent: this.widget
-                                .highlightStart,
-                            endPercent: this.widget.highlightEnd,
-                            width: 40.0
-                        )
-                    ),
-                  ),
-                  new Center(
-                    //   aspectRatio: 1.0,
-                      child: new Container(
-                          height: constraints.maxWidth,
-                          width: double.infinity,
-                          padding: const EdgeInsets.all(20.0),
-                          child: new Stack(
-                              fit: StackFit.expand,
-                              children: <Widget>[
-                                new CustomPaint(
-                                  painter: new HandPainter(
-                                      value: val,
-                                      start: this.start,
-                                      end: this.end,
-                                      color: this.widget.themeData.accentColor),
+    int start;
+    int end;
+    double highlightStart;
+    double highlightEnd;
+    PublishSubject<double> eventObservable;
+    
+    double val = 0.0;
+    double newVal;
+    AnimationController percentageAnimationController;
+    
+    
+    _SpeedOMeterState(int start, int end, double highlightStart, double highlightEnd, PublishSubject<double> eventObservable) {
+        this.start = start;
+        this.end = end;
+        this.highlightStart = highlightStart;
+        this.highlightEnd = highlightEnd;
+        this.eventObservable = eventObservable;
+        
+        percentageAnimationController = new AnimationController(
+            vsync: this,
+            duration: new Duration(milliseconds: 1000)
+        )
+            ..addListener((){
+                setState(() {
+                    val = lerpDouble(val,newVal,percentageAnimationController.value);
+                });
+            });
+        this.eventObservable.listen((value) => reloadData(value));
+    }
+    
+    reloadData(double value){
+        print(value);
+        newVal = value;
+        percentageAnimationController.forward(from: 0.0);
+    }
+    
+    @override
+    Widget build(BuildContext context) {
+        return new Center(
+            child: new LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                    return new Container(
+                        height: constraints.maxWidth,
+                        width: constraints.maxWidth,
+                        child: new Stack(
+                            fit: StackFit.expand,
+                            
+                            children: <Widget>[new Container(
+                                child: new CustomPaint(
+                                    foregroundPainter: new LinePainter(
+                                        lineColor:this.widget.themeData.backgroundColor,
+                                        completeColor: this.widget.themeData.primaryColor,
+                                        startValue: this.start,
+                                        endValue: this.end,
+                                        startPercent: this.widget
+                                            .highlightStart,
+                                        endPercent: this.widget.highlightEnd,
+                                        width: 40.0
+                                    )
                                 ),
-                              ]
-                          )
-                      )
-                  ),
-                  new Center(
-                    child: new Container(
-                      width: 30.0,
-                      height: 30.0,
-                      decoration: new BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: this.widget.themeData.backgroundColor,
-                      ),
-                    ),
-                  ),
-
-                  new CustomPaint(painter: new SpeedTextPainter(
-                      start: this.start,
-                      end: this.end,
-                      value: this.val)),
-
-                  ]
-              ),
-            );
-          }),
-    );
-  }
+                            ),
+                            new Center(
+                                //   aspectRatio: 1.0,
+                                child: new Container(
+                                    height: constraints.maxWidth,
+                                    width: double.infinity,
+                                    padding: const EdgeInsets.all(20.0),
+                                    child: new Stack(
+                                        fit: StackFit.expand,
+                                        children: <Widget>[
+                                            new CustomPaint(
+                                                painter: new HandPainter(
+                                                    value: val,
+                                                    start: this.start,
+                                                    end: this.end,
+                                                    color: this.widget.themeData.accentColor),
+                                            ),
+                                        ]
+                                    )
+                                )
+                            ),
+                            new Center(
+                                child: new Container(
+                                    width: 30.0,
+                                    height: 30.0,
+                                    decoration: new BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: this.widget.themeData.backgroundColor,
+                                    ),
+                                ),
+                            ),
+                            
+                            new CustomPaint(painter: new SpeedTextPainter(
+                                start: this.start,
+                                end: this.end,
+                                value: this.val)),
+                            
+                            ]
+                        ),
+                    );
+                }),
+        );
+    }
 }
